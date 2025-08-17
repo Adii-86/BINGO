4c2936f37d536c6181887039737771102dfdf2de
Adii <kumaradii731@gmain.com>
2025-08-17 18:27:10 +0530

    Used the MVC model for the after game logic

--- data/postGameData.js
+++ data/postGameData.js
@@ -8,6 +8,7 @@
     1) Game Board 5x5
     2) Row Cutted similarly Column Cutted also Diagonal
     3) Bingo Cutted Number
+    4) Clicked cell Board
 
 */
 
@@ -16,6 +17,9 @@ export let postGameData = JSON.parse(localStorage.getItem("postGameData"));
 if (!postGameData) {
   postGameData = new Object();
   postGameData.gameBoard = new Array(5).fill(0).map(() => new Array(5).fill(0));
+  postGameData.currBoard = new Array(5)
+    .fill(0)
+    .map(() => new Array(5).fill(""));
   postGameData.rowCutted = new Array(5).fill(0);
   postGameData.colCutted = new Array(5).fill(0);
   postGameData.diaCutted = new Array(2).fill(0);
@@ -24,4 +28,33 @@ if (!postGameData) {
 
 export function saveToLocalStg() {
   localStorage.setItem("postGameData", JSON.stringify(postGameData));
+}
+
+function updateBingoCut() {
+  let cnt = 0;
+  for (let i = 0; i < 5; i++) {
+    cnt += postGameData.rowCutted[i] === 5 ? 1 : 0;
+    cnt += postGameData.colCutted[i] === 5 ? 1 : 0;
+  }
+
+  if (postGameData.diaCutted[0] === 5) {
+    cnt++;
+  }
+  if (postGameData.diaCutted[1] === 5) {
+    cnt++;
+  }
+  postGameData.bingoCutted = cnt;
+}
+
+export function updateCut(i, j) {
+  postGameData.rowCutted[i]++;
+  postGameData.colCutted[j]++;
+  if (i === j) {
+    postGameData.diaCutted[0]++; // backward slash diagonal
+  }
+  if (i + j === 4) {
+    postGameData.diaCutted[1]++; // forward slash diagonal
+  }
+  updateBingoCut();
+  saveToLocalStg();
 }
--- scripts/afterGame.js
+++ scripts/afterGame.js
@@ -1,6 +1,6 @@
-import { postGameData } from "../data/postGameData.js";
+import { postGameData, updateCut } from "../data/postGameData.js";
 
-export function renderAfterStart() {
+function generateHTML() {
   const container = document.querySelector(".js-container");
   container.innerHTML = `      
     <div class="header js-header"></div>
@@ -10,8 +10,16 @@ export function renderAfterStart() {
   let headerHTML = "";
   const header = document.querySelector(".js-header");
   const headerElems = ["B", "I", "N", "G", "O"];
+  let cutted = postGameData.bingoCutted;
   headerElems.forEach((elem) => {
-    headerHTML += `<div class="header-cell">${elem}</div>`;
+    const [clicked, visible] = cutted > 0 ? ["clicked", "visible"] : ["", ""];
+    headerHTML += `
+      <div class="header-cell ${clicked}">
+        ${elem}
+        <img src="Images/cut.png" class="x-mark ${visible}"/>
+      </div>
+    `;
+    cutted--;
   });
 
   header.innerHTML = headerHTML;
@@ -19,23 +27,69 @@ export function renderAfterStart() {
   const gameBoard = postGameData.gameBoard;
   let gameContainerHTML = "";
   const gameContainer = document.querySelector(".js-game-container");
+  const currBoard = postGameData.currBoard;
 
   for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
+      const [clicked, visible] =
+        currBoard[i][j] === "clicked" ? ["clicked", "visible"] : ["", ""];
+
       gameContainerHTML += `
-        <div class="cell">
+        <div class="cell ${clicked}" data-row="${i}" data-col="${j}">
         ${gameBoard[i][j]}
-        <img src="Images/xmark.svg" class="x-mark"/>
-        </div>`;
+        <img src="Images/xmark.svg" class="x-mark ${visible}"/>
+        </div>
+      `;
     }
   }
 
   gameContainer.innerHTML = gameContainerHTML;
+}
 
+function eventListners() {
   document.querySelectorAll(".cell").forEach((cell) => {
     cell.addEventListener("click", () => {
-      cell.querySelector(".x-mark").classList.add("visible");
-      cell.classList.add("clicked");
+      const currBoard = postGameData.currBoard;
+      const row = Number(cell.dataset.row),
+        col = Number(cell.dataset.col);
+      if (currBoard[row][col] === "") {
+        currBoard[row][col] = "clicked";
+        updateCut(row, col);
+        renderAfterStart();
+        if (endedGame()) {
+          showGameEndPopUp();
+        }
+      }
     });
   });
+}
+
+function endedGame() {
+  return postGameData.bingoCutted === 5;
+}
+
+function showGameEndPopUp() {
+  const popupHTML = `
+    <div class="popup-overlay">
+      <div class="popup">
+        <h2>You Win!</h2>
+        <button class="restart-btn js-restart-btn">Restart Game</button>
+      </div>
+    </div>
+  `;
+
+  document.body.innerHTML += popupHTML;
+
+  document.querySelector(".js-restart-btn").addEventListener("click", () => {
+    localStorage.clear(); // clear prev data for the new game
+    location.reload();
+  });
+}
+
+export function renderAfterStart() {
+  // Generating part
+  generateHTML();
+
+  // Event Listner Part
+  eventListners();
 }
